2d7c79e6f32408323e6a3b7c3668ef737f470234
Patrick O'Sullivan <posullivan@uwf.edu>
2018-11-28 16:51:32 -0600

    complete rewrite of main

--- src/Main.js
+++ src/Main.js
@@ -1,17 +1,30 @@
+import Home from "./Content/Home"
 import PropTypes from "prop-types"
 import React from "react"
-import { Card, CardHeader, CardBody, CardTitle } from "reactstrap"
+import { Card, CardBody, CardHeader, CardTitle } from "reactstrap"
 
-const Main = props => (
-  <main>
-    <Card>
-      <CardHeader>
-        <CardTitle>{props.page}</CardTitle>
-      </CardHeader>
-      <CardBody>Card body content.</CardBody>
-    </Card>
-  </main>
-)
+const Main = props => {
+  let pageContent = "Home"
+  switch (props.page) {
+    case "Home":
+      pageContent = <Home />
+      break
+
+    default:
+      pageContent = <Home />
+      break
+  }
+  return (
+    <main className="py-2">
+      <Card>
+        <CardHeader>
+          <CardTitle>{props.page}</CardTitle>
+        </CardHeader>
+        <CardBody>{pageContent}</CardBody>
+      </Card>
+    </main>
+  )
+}
 
 Main.propTypes = {
   page: PropTypes.string
