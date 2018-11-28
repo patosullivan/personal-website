273edeac64b0b769985e8eb33638cb6c65f0e19a
Patrick O'Sullivan <posullivan@uwf.edu>
2018-11-28 15:41:16 -0600

    Added bootstrap

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,17 @@
 import PropTypes from "prop-types"
 import React from "react"
+import { Card, CardHeader, CardBody, CardTitle } from "reactstrap"
 
-const Main = props => <main>{props.page} stuff.</main>
+const Main = props => (
+  <main>
+    <Card>
+      <CardHeader>
+        <CardTitle>{props.page}</CardTitle>
+      </CardHeader>
+      <CardBody>Card body content.</CardBody>
+    </Card>
+  </main>
+)
 
 Main.propTypes = {
   page: PropTypes.string
