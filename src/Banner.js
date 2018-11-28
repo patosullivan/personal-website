c5e94b25436472c155b53ca293c106b6b91c13f0
Patrick O'Sullivan <posullivan@uwf.edu>
2018-11-28 15:47:44 -0600

    Forgot the "s"

--- src/Banner.js
+++ src/Banner.js
@@ -4,7 +4,7 @@ import { Jumbotron } from "reactstrap"
 const Banner = () => (
   <Jumbotron>
     <h1 className="display-3">Patrick OSullivan</h1>
-    <p className="lead">Javascript (E6+) developer.</p>
+    <p className="lead">Javascript (ES6+) developer.</p>
   </Jumbotron>
 )
 
