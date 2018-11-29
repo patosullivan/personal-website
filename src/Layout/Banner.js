1e18457eb410bb12dc0234bd3eff4fe4accb3f99
Patrick <patrick@osullivan.io>
2018-11-28 18:58:33 -0600

    Updated banner

--- src/Layout/Banner.js
+++ src/Layout/Banner.js
@@ -2,9 +2,14 @@ import React from "react"
 import { Jumbotron } from "reactstrap"
 
 const Banner = () => (
-  <Jumbotron>
+  <Jumbotron className="mb-2">
     <h1 className="display-3">{"Patrick O'Sullivan"}</h1>
-    <p className="lead">Javascript (ES6+) developer.</p>
+    <p className="lead">Front-End Developer.</p>
+    <hr />
+    <p>
+      Developer specializing in frontend development using Javascript (ES6+),
+      React, and Typescript.
+    </p>
   </Jumbotron>
 )
 
