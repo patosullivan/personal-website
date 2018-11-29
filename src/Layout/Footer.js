f6f2cbe88ec2a9d895becab8d10010897b2adebd
Patrick <patrick@osullivan.io>
2018-11-28 19:20:01 -0600

    Added link to github repo

--- src/Layout/Footer.js
+++ src/Layout/Footer.js
@@ -4,7 +4,16 @@ const year = new Date().getFullYear()
 
 const Footer = () => (
   <footer className="bg-light border w-100 mx-0 px-2 py-1 mt-2 rounded">
-    <small className="text-muted">&copy; {year}</small>
+    <small className="text-muted">
+      &copy; {year} |{" "}
+      <a
+        className="text-muted"
+        href="https://github.com/patosullivan/personal-website"
+      >
+        Check out the repo for this web app on GitHub
+      </a>
+      .
+    </small>
   </footer>
 )
 
