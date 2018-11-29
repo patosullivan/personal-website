49c149c206cc5ef2b5894d4b6d8cc414dc9bf8ac
Patrick <patrick@osullivan.io>
2018-11-28 23:00:16 -0600

    Fleshed out contact page a bit.

--- src/Content/Contact.js
+++ src/Content/Contact.js
@@ -1,5 +1,29 @@
 import React from "react"
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { ListGroup, ListGroupItem } from "reactstrap"
 
-const Contact = () => <React.Fragment>Contact info.</React.Fragment>
+const Contact = () => (
+  <React.Fragment>
+    <ListGroup>
+      <ListGroupItem>
+        <a href="mailto:patrick@osullivan.io">
+          <FontAwesomeIcon icon={faEnvelope} /> Email
+        </a>
+      </ListGroupItem>
+      <ListGroupItem>
+        <a href="https://github.com/patosullivan">
+          <FontAwesomeIcon icon={faGithub} /> Github
+        </a>
+      </ListGroupItem>
+      <ListGroupItem>
+        <a href="https://linkedin.com/in/patosullivan">
+          <FontAwesomeIcon icon={faLinkedin} /> LinkedIn
+        </a>
+      </ListGroupItem>
+    </ListGroup>
+  </React.Fragment>
+)
 
 export default Contact
