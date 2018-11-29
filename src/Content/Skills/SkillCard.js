f52b572e5677007f07ce3ae2e9598b40e198571e
Patrick O'Sullivan <posullivan@uwf.edu>
2018-11-29 16:08:22 -0600

    Added icons to skills page Removed TypeScript because it's probably superfluous to list it.

--- src/Content/Skills/SkillCard.js
+++ src/Content/Skills/SkillCard.js
@@ -1,9 +1,13 @@
 import PropTypes from "prop-types"
 import React from "react"
-import { Card, CardBody, CardText, CardTitle } from "reactstrap"
+import { Card, CardBody, CardHeader, CardText, CardTitle } from "reactstrap"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 const SkillCard = props => (
   <Card>
+    <CardHeader>
+      <FontAwesomeIcon icon={props.icon} size="lg" />
+    </CardHeader>
     <CardBody>
       <CardTitle>{props.title}</CardTitle>
       <CardText>{props.text}</CardText>
@@ -13,7 +17,8 @@ const SkillCard = props => (
 
 SkillCard.propTypes = {
   title: PropTypes.string,
-  text: PropTypes.string
+  text: PropTypes.string,
+  icon: PropTypes.object
 }
 
 export default SkillCard
--- src/Content/Skills/Skills.js
+++ src/Content/Skills/Skills.js
@@ -1,31 +1,36 @@
 import React from "react"
 import SkillCard from "./SkillCard"
 import { CardGroup } from "reactstrap"
+import { faNode, faJsSquare, faReact } from "@fortawesome/free-brands-svg-icons"
 
 const Skills = () => {
   const skillArray = [
     {
       skill: "React",
-      text: "React 16+, including Hooks, Suspense, and other modern features."
+      text: "React 16+, including Hooks, Suspense, and other modern features.",
+      icon: faReact
     },
     {
       skill: "JavaScript",
-      text: "ES6+. Modern, efficient. JAMstack preferred."
-    },
-    {
-      skill: "TypeScript",
-      text: "Safe, robust, clean, and self-documenting."
+      text: "ES6+. Modern, efficient. JAMstack preferred.",
+      icon: faJsSquare
     },
     {
       skill: "Node",
-      text: "Fast JS backends/APIs with Express.js"
+      text: "Fast JS backends/APIs with Express.js",
+      icon: faNode
     }
   ]
 
   return (
     <CardGroup>
       {skillArray.map((skillObj, index) => (
-        <SkillCard key={index} title={skillObj.skill} text={skillObj.text} />
+        <SkillCard
+          key={index}
+          title={skillObj.skill}
+          text={skillObj.text}
+          icon={skillObj.icon}
+        />
       ))}
     </CardGroup>
   )
