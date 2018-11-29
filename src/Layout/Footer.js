b5f0c2aede494ff5cc71e0f620c2bc101114bde4
Patrick <patrick@osullivan.io>
2018-11-28 18:58:47 -0600

    Changed text on footer

--- src/Layout/Footer.js
+++ src/Layout/Footer.js
@@ -4,7 +4,7 @@ const year = new Date().getFullYear()
 
 const Footer = () => (
   <footer className="bg-light border w-100 mx-0 px-2 py-1 mt-2 rounded">
-    &copy; {year}
+    <small className="text-muted">&copy; {year}</small>
   </footer>
 )
 
