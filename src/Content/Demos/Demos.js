3e6d96f124221baede700f1c8662f727249ea0ff
Patrick <patrick@osullivan.io>
2018-12-04 22:58:49 -0600

    updated demos details

--- src/Content/Demos/Demos.js
+++ src/Content/Demos/Demos.js
@@ -1,24 +1,25 @@
 import React from "react"
 import DemoCard from "./DemoCard"
-import { CardGroup } from "reactstrap"
+import {CardGroup} from "reactstrap"
 
 const Demos = () => {
   const demoArray = [
     {
-      demo: "Todo App",
-      text: "Simple todo app.",
-      link: "#",
-      ghLink: "#"
+      demo: "Andrea's blog",
+      text:
+        "A website and blog for my wife, Andrea. Built using React for the frontend and strapi for the backend. The frontend is deployed via a contiuous integration process on Netlify, the backend is deployed via docker-machine on Digital Ocean.",
+      link: "http://andrea.osullivan.io",
+      ghLink: "https://github.com/patosullivan/andrea-blog"
     },
     {
       demo: "Demo App #2",
-      text: "Another cool app.",
+      text: "Coming soon.",
       link: "#",
       ghLink: "#"
     },
     {
       demo: "Demo App #3",
-      text: "Check it out!",
+      text: "Coming soon.",
       link: "#",
       ghLink: "#"
     }
