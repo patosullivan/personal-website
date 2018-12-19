1be4f41cbcc0bac017284656735d3888d09cc17c
Patrick <patrick@osullivan.io>
2018-12-19 15:33:28 -0600

    got a bit more specific with media queries.

--- src/Layout/Banner.js
+++ src/Layout/Banner.js
@@ -19,14 +19,31 @@ const nameStyle = css`
   }
 `
 
+const summaryStyle = css`
+  @media (max-width: 375px) and (min-width: 360px) {
+    font-size: 1.5rem;
+  }
+  @media (max-width: 414px) and (min-width: 411px) {
+    font-size: 1.6rem;
+  }
+`
+
+const secondarySummaryStyle = css`
+  @media (max-width: 320px) {
+    font-size: 0.95rem;
+  }
+`
+
 const Banner = () => (
   <Jumbotron className="my-2 text-center" css={jumboStyle}>
     <h1 css={nameStyle} className="display-4 text-monospace">
       {"Patrick O'Sullivan"}
     </h1>
-    <p className="lead text-monospace">Husband, Father, Front-End Developer.</p>
+    <p css={summaryStyle} className="lead text-monospace">
+      Husband, Father, Front-End Developer.
+    </p>
     <hr />
-    <p>JavaScript (ES6+), React, and Node.</p>
+    <p css={secondarySummaryStyle}>JavaScript (ES6+), React, and Node.</p>
   </Jumbotron>
 )
 
