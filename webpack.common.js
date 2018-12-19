be2e97f8d323b8176f4e3ebe98d89a05d1d3a856
Patrick <patrick@osullivan.io>
2018-12-19 11:57:06 -0600

    reduced bundle size, added .swp to gitignore

--- webpack.common.js
+++ webpack.common.js
@@ -11,7 +11,7 @@ module.exports = {
       test: /\.(jpe?g|png|gif|svg)$/i,
       plugins: [
         imageminMozjpeg({
-          quality: 80,
+          quality: 75,
           progessive: true
         })
       ]
