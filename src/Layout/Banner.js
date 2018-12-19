05f31620b83b46d20422ded4ca90ac58accc73b8
Patrick <patrick@osullivan.io>
2018-12-19 14:30:45 -0600

    made some accomadtions for small phones

--- src/Layout/Banner.js
+++ src/Layout/Banner.js
@@ -1,18 +1,29 @@
 import React from "react"
 import { Jumbotron } from "reactstrap"
+import { css } from "@emotion/core"
 
-const jumboStyle = {
-  backgroundImage: "url(./foggyMountains.jpg)",
-  backgroundPosition: "center",
-  backgroundRepeat: "no-repeat",
-  backgroundSize: "cover",
-  padding: "1rem 1rem",
-  marginBottom: "0rem"
-}
+const jumboStyle = css`
+  background-image: url(./foggyMountains.jpg);
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  padding: 1rem 1rem;
+  marginbottom: 0rem;
+`
+
+const nameStyle = css`
+  @media (device-width: 320px), (device-width: 375px) {
+    font-size: 2.5rem;
+    font-weight: 300;
+    line-height: 1.2;
+  }
+`
 
 const Banner = () => (
   <Jumbotron className="my-2 text-center" css={jumboStyle}>
-    <h1 className="display-4 text-monospace">{"Patrick O'Sullivan"}</h1>
+    <h1 css={nameStyle} className="display-4 text-monospace">
+      {"Patrick O'Sullivan"}
+    </h1>
     <p className="lead text-monospace">Husband, Father, Front-End Developer.</p>
     <hr />
     <p>JavaScript (ES6+), React, and Node.</p>
--- src/Layout/Nav.js
+++ src/Layout/Nav.js
@@ -2,12 +2,20 @@ import { Nav as RSNav, NavItem, NavLink } from "reactstrap"
 import PropTypes from "prop-types"
 import React from "react"
 import { NavLink as RRNavLink } from "react-router-dom"
+import { css } from "@emotion/core"
+
+const navLinkStyle = css`
+  @media (device-width: 320px), (device-width: 375px) {
+    padding: 0.5rem 0.5rem;
+  }
+`
 
 const Nav = props => (
   <RSNav pills fill className="border rounded py-0">
     {props.pages.map((page, index) => (
       <NavItem key={index}>
         <NavLink
+          css={navLinkStyle}
           exact
           to={page == "About" ? "/" : `/${page}`}
           activeClassName="active"
