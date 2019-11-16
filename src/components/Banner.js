8d95e908660ea77ab942e8f4176acb88aed639d4
Patrick O'Sullivan <patrick@osullivan.io>
2019-11-16 10:40:23 -0600

    remove unnecessary import add letter-spacing

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,6 @@
 import React from "react"
 import { Jumbotron } from "reactstrap"
 import { css } from "@emotion/core"
-import foggyMountains from "./foggyMountains.jpg"
 
 const jumboStyle = css`
   background-image: url("./imgs/foggyMountains-1140.jpg");
@@ -23,21 +22,25 @@ const nameStyle = css`
     font-size: 2.5rem;
     font-weight: 300;
     line-height: 1.2;
+    letter-spacing: -1px;
   }
 `
 
 const summaryStyle = css`
   @media (max-width: 375px) and (min-width: 360px) {
     font-size: 1.5rem;
+    letter-spacing: -1px;
   }
   @media (max-width: 414px) and (min-width: 411px) {
     font-size: 1.6rem;
+    letter-spacing: -1px;
   }
 `
 
 const secondarySummaryStyle = css`
   @media (max-width: 320px) {
     font-size: 0.95rem;
+    letter-spacing: -1px;
   }
 `
 
