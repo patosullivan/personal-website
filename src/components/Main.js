4eb6a4331dbb69395dd76b347f080efb36ad012c
Patrick O'Sullivan <patrick@osullivan.io>
2020-06-02 16:31:23 -0500

    Add dynamic imports

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,23 @@
-import About from "../views/About"
 import React from "react"
 import { Card, CardBody } from "reactstrap"
-import Contact from "../views/Contact"
-import Skills from "../views/Skills/Skills"
-import Demos from "../views/Demos/Demos"
 import { Switch, Route } from "react-router-dom"
+
+const About = React.lazy(() => import("../views/About"))
+const Contact = React.lazy(() => import("../views/Contact"))
+const Skills = React.lazy(() => import("../views/Skills/Skills"))
 
 const Main = () => {
   return (
     <main className="py-1">
       <Card>
         <CardBody className="p-2 mx-3">
-          <Switch>
-            <Route exact path="/" component={About} />
-            <Route path="/contact" component={Contact} />
-            <Route path="/skills" component={Skills} />
-            <Route path="/demos" component={Demos} />
-          </Switch>
+          <React.Suspense fallback={"Loding..."}>
+            <Switch>
+              <Route exact path="/" component={() => <About />} />
+              <Route path="/contact" component={() => <Contact />} />
+              <Route path="/skills" component={() => <Skills />} />
+            </Switch>
+          </React.Suspense>
         </CardBody>
       </Card>
     </main>
