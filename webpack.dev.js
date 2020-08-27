b5ddeb6b1f9e1d7125026593ee8b1afd0fe1a50d
Patrick O'Sullivan <patrick@headway.io>
2020-08-27 10:20:31 -0500

    Switch to webpack-dev-server

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,14 +1,20 @@
 const webpack = require("webpack")
 const merge = require("webpack-merge")
 const common = require("./webpack.common.js")
+const path = require("path")
 
 module.exports = merge(common, {
   mode: "development",
   watch: true,
   devtool: "inline-source-map",
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist"),
+    hot: true,
+    overlay: true,
+  },
   plugins: [
     new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify("development")
-    })
-  ]
+      "process.env.NODE_ENV": JSON.stringify("development"),
+    }),
+  ],
 })
